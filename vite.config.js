7c06de40d8c81a844679fa6fb5d1f9b76e5e0a1d
LabheshBhurewal <bhurewallabhesh05@gmail.com>
2024-03-11 14:43:40 +0530

    initialize the Project

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   // Add Your Github Repository Name Here
-  base: "/React-Template/",
+  base: "/CineVista/",
   plugins: [react()],
 });
